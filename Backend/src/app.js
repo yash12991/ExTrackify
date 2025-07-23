7e7e45f21b83a420e851d49fb5be6f5781d2278d
Sonawane Yash Prashant <YASHSONAWANE129910@GMAIL.COM>
2025-07-23 20:33:26 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,13 +13,18 @@ try {
 }
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173"||"https://ex-trackify-vw3z.vercel.app",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.CORS_ORIGIN || "http://localhost:5173"||"https://ex-trackify-vw3z.vercel.app",
+//     credentials: true,
+//   })
+// );
 // app.use(cors());
+app.use(cors({
+  origin: '*',         /
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+}));
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
