6b1721a2b55c2a9ece7c2dfa5f5f6dd372e00f5f
Sonawane Yash Prashant <YASHSONAWANE129910@GMAIL.COM>
2025-07-23 20:08:13 +0530

    Update axios.js

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://extrackify.onrender.com/api/v",
+  baseURL: "https://extrackify.onrender.com/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
