0599a30d5d291bf80cbd7be0301740935e8c7ea3
Sonawane Yash Prashant <YASHSONAWANE129910@GMAIL.COM>
2025-07-23 20:36:07 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 // );
 // app.use(cors());
 app.use(cors({
-  origin: '*',         /
+  origin: '*',         
   methods: ['GET', 'POST', 'PUT', 'DELETE'],  
   allowedHeaders: ['Content-Type', 'Authorization'], 
 }));
