44b18d856e4fd5f82ed09e900177227ec9bda416
Sonawane Yash Prashant <YASHSONAWANE129910@GMAIL.COM>
2025-07-23 19:02:57 +0530

    added security

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,9 +9,7 @@ app.use(express.json());
 //     credentials: true,
 //   })
 // );
-app.use(
-  cors()
-);
+app.use(cors());
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
