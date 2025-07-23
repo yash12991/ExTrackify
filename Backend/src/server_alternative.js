66d0f13c412543e1ab37279eb8c803bcede1448d
Sonawane Yash Prashant <YASHSONAWANE129910@GMAIL.COM>
2025-07-23 19:52:07 +0530

    initiated docker

---
+++ Backend/src/server_alternative.js
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import { app } from "./app_alternative.js";
+import { connectDB } from "./db/index.js";
+import { startRecurringExpenseJob } from "./cronJobs/cronJobs.js";
+import "./cronJobs/sipScheduler.js"; // Import SIP reminder cron jobs
+
+const PORT = process.env.PORT || 3001;
+
+connectDB()
+  .then(() => {
+    startRecurringExpenseJob();
+
+    app.on("error", (error) => {
+      console.log("Express app error: ", error);
+      throw error;
+    });
+
+    app.listen(PORT, () => {
+      console.log(`⚙️ Server is running at port: ${PORT}`);
+      console.log(
+        `🌐 CORS enabled for: ${
+          process.env.CORS_ORIGIN || "http://localhost:5173"
+        }`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+    process.exit(1);
+  });
